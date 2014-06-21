b3f705f91cbf487fdeb2b5bab6b645880c636bd2
Sudharshan S <sudharsh@gmail.com>
2014-06-22 01:20:57 +0530

    Test cases for ParsePlan

---
+++ henchman/plan_test.go
@@ -0,0 +1,59 @@
+package henchman
+
+import "testing"
+
+func TestParsePlanWithoutOverrides(t* testing.T) {
+	plan_string := `---
+hosts:
+  - 127.0.0.1
+tasks:
+  - name: Sample task that does nothing
+    action: ls -al
+  - name: Second task
+    action: echo 'foo'
+ `
+	plan, err := ParsePlan([]byte(plan_string), nil)
+	if err != nil {
+		panic(err)
+	}
+	if len(plan.Hosts) != 1 {
+		t.Errorf("Number of hosts mismatch. Parsed %d hosts instead\n", len(plan.Hosts))
+	}
+	if len(plan.Tasks) != 2 {
+		t.Errorf("Numnber of tasks mismatch. Parsed %d tasks instead\n", len(plan.Tasks))
+	}
+}
+
+
+func TestParsePlanWithOverrides(t* testing.T) {
+		plan_string := `---
+vars:
+  service: foo
+hosts:
+  - 127.0.0.1
+tasks:
+  - name: Sample task that does nothing
+    action: ls -al
+  - name: Second task
+    action: echo 'foo'
+ `
+	tv := make(TaskVars)
+	tv["service"] = "overridden_foo"
+	
+	plan, err := ParsePlan([]byte(plan_string), tv)
+	if err != nil {
+		panic(err)
+	}
+	if len(plan.Hosts) != 1 {
+		t.Errorf("Number of hosts mismatch. Parsed %d hosts instead\n", len(plan.Hosts))
+	}
+	if len(plan.Tasks) != 2 {
+		t.Errorf("Numnber of tasks mismatch. Parsed %d tasks instead\n", len(plan.Tasks))
+	}
+	if plan.Vars["service"] != "overridden_foo" {
+		t.Error("Plan vars 'service' should have been 'foo'")
+	}
+}
+	
+	
+	
