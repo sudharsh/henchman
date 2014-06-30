280a0f52c4d78a4b4f80b5603310b479528ca1cb
Sudharshan S <sudharsh@gmail.com>
2014-06-30 23:43:34 +0530

    Test cases for the machine context

--- lib/task_test.go
+++ lib/task_test.go
@@ -5,15 +5,18 @@ import (
 )
 
 func TestPrepareTask(t *testing.T) {
-	task := Task{"fake-uuid", "The {{ vars.variable1 }}", "The {{ vars.variable2 }}", false}
+	task := Task{"fake-uuid", "The {{ vars.variable1 }}", "{{ vars.variable2 }}:{{ machine.Hostname }}", false}
+	machine := Machine{"foobar", nil}
+
 	vars := make(TaskVars)
 	vars["variable1"] = "foo"
 	vars["variable2"] = "bar"
-	task.Prepare(&vars)
+	task.prepare(&vars, &machine)
+
 	if task.Name != "The foo" {
 		t.Errorf("Template execution for Task.Name failed. Got - %s\n", task.Name)
 	}
-	if task.Action != "The bar" {
+	if task.Action != "bar:foobar" {
 		t.Errorf("Template execution for Task.Action failed. Got - %s\n", task.Action)
 	}
 }
